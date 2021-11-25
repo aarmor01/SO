d346253a0576b25070f7f8a211bcc2826fe73443
aarmor01 <62306290+aarmor01@users.noreply.github.com>
2021-11-25 11:46:38 +0100

    Apartado 1
    sencillo

--- P3/FicherosP3/Projects/MySystem/my_system.c
+++ P3/FicherosP3/Projects/MySystem/my_system.c
@@ -1,5 +1,29 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int my_system(const char* command) {
+	pid_t child_pid;
+	int child;
+
+	child_pid = fork());
+	if (child_pid == 0) { //child
+		// NULL defines that there are no more args on the execl
+		execl("/bin/bash", "/bin/bash", "-c", command, NULL); 
+		exit(-1);
+	} 
+	else { // parent
+		// waits to any child process to end
+		// &child returns the child process that has ended
+		wait(&child);
+	}
+
+	// exclusive to the parent process
+	return child;
+}
 
 int main(int argc, char* argv[])
 {
@@ -8,6 +32,6 @@ int main(int argc, char* argv[])
 		exit(1);
 	}
 
-	return system(argv[1]);
+	return my_system(argv[1]);
 }
 
